0d41a13f7079f721d19e964b282a47b0ba0c03ee
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-01-16 05:20:43 +0200

    temp

--- web/src/templates/swiftBank/swiftBank.swift
+++ web/src/templates/swiftBank/swiftBank.swift
@@ -22,11 +22,47 @@ struct Home: View {
     var proxy: ScrollViewProxy
     var size: CGSize
     var safeArea: EdgeInsets
+    /// View Properties
+    @State private var activePage: Int = 1
+    @State private var myCards: [Card]= sampleCards
 
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack(spacing: 10) {
                 ProfileCard()
+
+                ///Indicator
+                Capsule()
+                .fill(.gray.opacity(0.2))
+                .frame(width:50, height:5)
+                .padding(.vertical, 5)
+                
+                ///Page Tab View Height Based on Screen Height
+                let pageHeight = size.height * 0.65
+                ///PageTab View
+                //To Keep track of minY
+                GeometryReader {
+                    let geometry = $0.frame(in: .global)
+
+                    TabView(selection: $activePage) {
+                        ForEach(myCards) { card in
+                            ZStack
+                            {
+                                if card.isFirstBlankCard {
+                                    Rectangle() 
+                                    .fill(.clear) 
+                                } else  {
+                                    /// Card View
+                                    CardView(card: card)
+                                }
+                            }
+                            ///Page Tag (Index)
+                            .tag(index(card))
+                        }
+                    }
+                    .tabViewStyle(.page(indexDisplayMode: .never))
+                }
+                .frame(height: pageHeight)
             }
             .padding(.top, safeArea.top + 15)
             .padding(.bottom, safeArea.bottom + 15)
@@ -57,7 +93,13 @@ struct Home: View {
             RoundedRectangle(cornerRadius:35, style: .continuous)
             .fill(Color("Profile"))
         )
+        .padding(.horizontal, 30)
         
+    }
+
+    ///Returns Index for Given Card
+    func index(_ of: Card) -> Int {
+        return myCards.firstIndex(of: of) ?? 0
     }
 }
 
@@ -73,4 +115,21 @@ struct ContentView: View {
             .ignoresSafeArea()
         }
     }
+}
+
+///Card View
+struct CardView: View {
+    var card: Card
+
+    var body: some View {
+        GeometryReader {
+            let size = $0.size
+
+            VStack(spacing: 0)
+            {
+                
+            }
+        }
+    }
+    
 }
