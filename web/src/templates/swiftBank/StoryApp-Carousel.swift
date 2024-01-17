a7d5244184383d3c14efb4963fe0a508c6207e4e
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-01-17 21:44:49 +0200

    temp

--- web/src/templates/swiftBank/StoryApp-Carousel.swift
+++ web/src/templates/swiftBank/StoryApp-Carousel.swift
@@ -1,19 +1,57 @@
 struct Home: View {
+    @State private var index=0
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack
             {
                 HStack
                 {
+                    Button(action: {}) {
                     Image("menu")
                     .renderingMode(.template)
                     .foregroundColor(.white)
+                    }
+                    
+                    Spacer()
 
+                    Button(action: {}) {
                     Image("search")
                     .renderingMode(.template)
                     .foregroundColor(.white)
+                    }
                 }
+                .padding()
+
+                HStack
+                {
+                    Text("Trending")
+                    .font(.system(size: 40,weight: .bold, design: .rounded)))
+                    .foregroundColor(.white)
+
+                    Spacer(minLength: 0)
+
+                    Button(action: {}) {
+                        Image("dots")
+                        .renderingMode(.template)
+                        .foregroundColor(.white)
+                        .rotationEffect(.init(degrees: 90))
+
+
+                    }
+                }
+                .padding(.horizontal)
+
+                HStack
+                {
+                    
+                }
+
+                Spacer()
             }
         }
+        .background(
+            LinearGradient(gradient: .init(colors: [Color("top"), Color("bottom")]),startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+        )
     }
 }
