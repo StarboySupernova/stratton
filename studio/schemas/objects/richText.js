81b053df48704a1a38ab746da28f50bd47824ce5
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-01-23 21:42:46 +0200

    customCode type removed

--- studio/schemas/objects/richText.js
+++ studio/schemas/objects/richText.js
@@ -6,36 +6,25 @@ export default {
     {
       type: 'block',
       title: 'Block',
-      // Styles let you set what your user can mark up blocks with. These
-      // corrensponds with HTML tags, but you can set any title or value
-      // you want and decide how you want to deal with it where you want to
-      // use your content.
       styles: [
-        // setting up and segregating the only styles that we want when formatting text in creating a new blog
         { title: 'Normal', value: 'normal' },
         { title: 'H1', value: 'h1' },
         { title: 'H3', value: 'h3' },
       ],
       lists: [
-        // setting up and customizing the lists we will see in creating blog posts. Title is what the user will see when they hover over the controls
         { title: 'Bullet List', value: 'bullet' },
         { title: 'Numbered List', value: 'number' },
       ],
-      // Marks let you mark up inline text in the block editor.
       marks: {
-        // Decorators usually describe a single property – e.g. a typographic
-        // preference or highlighting by editors.
         decorators: [
-          // setting up and segregating the only text decorators we want to see in creating a blog post
           { title: 'Bolden', value: 'strong' },
-          { title: 'Italicize', value: 'em' }, // em from the html tag
+          { title: 'Italicize', value: 'em' },
         ],
-        // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [
           {
             title: 'Create link',
             name: 'link',
-            type: 'object', // this is line the Any type in Swift
+            type: 'object',
             fields: [
               {
                 title: 'Link URL',
@@ -51,9 +40,6 @@ export default {
           },
         ],
       },
-    },
-    {
-      type: 'customCode',
     },
     {
       type: 'customImage',
