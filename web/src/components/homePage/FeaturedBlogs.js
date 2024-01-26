e5168e59467ebaf5eb56883d446ab996e2d97825
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-01-26 17:12:18 +0200

    commented out info

--- web/src/components/homePage/FeaturedBlogs.js
+++ web/src/components/homePage/FeaturedBlogs.js
@@ -48,3 +48,18 @@ function FeaturedBlogs() {
 }
 
 export default FeaturedBlogs;
+
+/* Patricia holds a Masters degree in Environmental Management and Society from University of Pretoria , an Occupational Health and Safety training from NOSSA and a certificate in Incident Investigation& Audit – Environmental law from Ministry of Environment and Tourism . She is a determined, hard-working and analytically minded Environmental analyst. In her Master’s degree she is Specializing in Environmental law .In that regard she has proficiency in policy analysis implementation, application and compliance. She has been resourceful in Auditing projects requiring ISO140001; ISO9000; ISO18000 Environmental Health and safety policy compliance. Her professional experience in the past 10 years includes being a Business developer, Environmental Consultant, Occupational Health and Safety officer, Public Relations Officer, Events management at various consultancies that dealt a lot with Policies and Governance ,Engineering, Construction, Mining and Non-governmental Charitable Hospitality Organisations to name a few.
+
+Patricia is not only well acquainted in Environmental Management processes in compliance to internationally best management Practices including the Equator Principles,World Bank, IFC Performance Standards and Environmental, Health and Safety Guidelines but assesses audits compliance avoiding any liability to the organisation .She has10 years of experience in the GIS, Environmental and Social Impact Assessments Occupational Health and safety and has been involved in formal environmental authorization processes for the residential, commercial, industrial and mining sectors,including World Bank Category ‘A’ environmental impact assessments She has also been active in Projects that require Community liaison and Public Participation in various projects in Southern East and West Africa. Patricia has the drive the business acumen and natural strategies to drive any company’s goals to realisation */
+// categories
+// Environmental Analysis, Consultancy, Impact Assessment & Non-governmental Charitable Hospitality
+// Occupational Health and Safety
+//  Policies and Governance
+
+// principles
+/// Equator Principles,World Bank, IFC Performance Standards and Environmental, Health and Safety Guidelines, audit assessment,  GIS, Environmental and Social Impact Assessments Occupational Health and safety
+/// Community liaison and Public Participation
+
+/// colors
+/// #1ff4b4, #2c313e
