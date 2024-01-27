38e93d50a375c97f00f6ec25fd8d0b1e5e8c4287
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-01-27 05:58:23 +0200

    service list preliminary changes

---
+++ web/src/templates/service-list.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import PageSpace from '../components/PageSpace';
+import SEO from '../components/seo';
+import PageHeader from '../components/PageHeader';
+import CategoryGrid from '../components/category/CategoryGrid';
+import Pagination from '../components/Pagination';
+
+export const ServiceListQuery = graphql`
+  query servicesQuery($limit: Int!, $offset: Int!) {
+    allSanityService(
+      sort: { fields: _createdAt, order: DESC }
+      limit: $limit
+      skip: $offset
+    ) {
+      nodes {
+        id
+        title
+        slug {
+          current
+        }
+        _rawDescription
+      }
+    }
+  }
+`;
+
+function Services({ data, pageContext }) {
+  const { currentPage, numberOfPages } = pageContext;
+  const services = data.allSanityService.nodes;
+
+  return (
+    <>
+      <SEO title="Services" />
+      <PageSpace top={80} bottom={100}>
+        <div className="container">
+          <PageHeader
+            title="All Services"
+            description="Elevate your projects with Mwenje Geology and Environmental Consultants, your go-to partner for comprehensive and expert geophysical solutions across diverse environments in Sub-Saharan Africa, ensuring success at every stage of development."
+          />
+          <CategoryGrid categories={services} />
+          {numberOfPages > 1 && (
+            <Pagination
+              currentPage={currentPage}
+              numberOfPages={numberOfPages}
+              baseURL="/services"
+            />
+          )}
+        </div>
+      </PageSpace>
+    </>
+  );
+}
+
+export default Services;
